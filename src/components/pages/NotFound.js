c7ff37547b605c0b1e8001cbac2e840361073403
Benjamin Catovic <njobashhh@Benjo.local>
2018-08-29 20:35:04 +0200

    initial commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">Page not found</h1>
+      <p className="lead">Sorry, ali odjebi odavdje mater ti jebem</p>
+    </div>
+  );
+};
